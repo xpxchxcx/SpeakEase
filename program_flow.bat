8e211058bb40e7ed1646b9a6f46370c18bc3a701
xpxchxcx <118047971+xpxchxcx@users.noreply.github.com>
2023-01-30 23:07:15 +0800

    Fixed Project Flow from static directories to work with current project structure.

--- program_flow.bat
+++ program_flow.bat
@@ -1,14 +1,13 @@
 @echo off
-cd C:\Users\mynam\Downloads\AISG_Challenge-main\raw_video_pipeline
+cd %~dp0raw_video_pipeline
 peekingduck run
 echo raw video capture complete
 pause
-python C:\Users\mynam\Downloads\AISG_Challenge-main\raw_transfer.py
+cd ..
+python raw_transfer.py
 echo source as raw video on analysis pipeline complete
 pause
-
-cd C:\Users\mynam\Downloads\AISG_Challenge-main\analysis_video_pipeline
+cd %~dp0analysis_video_pipeline
 peekingduck run
-
 echo analysis complete
 pause
--- raw_transfer.py
+++ raw_transfer.py
@@ -1,13 +1,17 @@
 import os
 import re
 
+ROOT_DIR = os.path.abspath(os.curdir)
+MP3_DIR = ROOT_DIR + "./raw_video_pipeline/raw_videos"
+YML_DIR = ROOT_DIR + "./analysis_video_pipeline/pipeline_config.yml"
+
 # Find the newest .mp3 file in the directory
-directory = r'C:\Users\mynam\Downloads\AISG_Challenge-main\raw_video_pipeline\raw_videos'
+directory = MP3_DIR
 newest_mp3 = max(os.listdir(directory), key=lambda f: os.path.getctime(os.path.join(directory, f)))
 newest_mp3_path = os.path.join(directory, newest_mp3).replace("\\", "\\\\")
 
 # Read the contents of the yml file
-with open(r'C:\Users\mynam\Downloads\AISG_Challenge-main\analysis_video_pipeline\pipeline_config.yml', 'r') as file:
+with open(YML_DIR, 'r') as file:
     contents = file.read()
 
 # Use regular expression to replace only the text after "source: " on the specific line
@@ -15,6 +19,6 @@ contents = re.sub(r"source:.*", f'source: {newest_mp3_path}', contents)
 
 
 # Write the modified contents back to the yml file
-with open(r'C:\Users\mynam\Downloads\AISG_Challenge-main\analysis_video_pipeline\pipeline_config.yml', 'w') as file:
+with open(YML_DIR, 'w') as file:
     file.write(contents)
 
